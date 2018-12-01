923de876d05e53671985239bd60a32cf6353331f
Mark Apinis <mapinis2019@gmail.com>
2018-12-01 14:00:37 -0500

    makeDeck.js

--- server/lib/deck.js
+++ server/lib/deck.js
@@ -1,5 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports.makeDecks = () => {
-  return [];
+  const promptsText = fs.readFileSync(
+    path.join(__dirname, "../../content/prompts.txt"),
+    { encoding: "utf-8" }
+  );
+  const responsesText = fs.readFileSync(
+    path.join(__dirname, "../../content/responses.txt"),
+    { encoding: "utf-8" }
+  );
+
+  const prompts = [];
+  promptsText.split("\n").forEach((elm, index) => {
+    prompts.push({
+      id: index + 1,
+      text: elm
+    });
+  });
+
+  const responses = [];
+  responsesText.split("\n").forEach((elm, index) => {
+    responses.push({
+      id: index + 1,
+      text: elm
+    });
+  });
+
+  return {
+    prompts,
+    responses
+  };
 };
 
 module.exports.allCards = module.exports.makeDecks();
