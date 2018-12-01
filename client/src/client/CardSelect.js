5b9e07d585f9483d4c1865dad4abf70b09ee38dc
AaronCGoidel <agoidel2019@mka.org>
2018-12-01 18:41:20 -0500

    some card formatting

--- client/src/client/CardSelect.js
+++ client/src/client/CardSelect.js
@@ -1,20 +1,19 @@
 import React from "react";
+import Card from '../common/Card';
 
 const CardSelect = ({ cards, onSelect }) => {
   return (
-    <div>
+    <div style={{maxHeight: '100vh'}}>
       <h1>Pick a card</h1>
+      <div style={{
+        overflowX: 'scroll',
+        height: '400px',
+        whiteSpace: 'nowrap'
+      }}>
       {cards.map(e => (
-        <div key={e.id}>
-          <button
-            onClick={() => {
-              onSelect(e);
-            }}
-          >
-            {e.text}
-          </button>
-        </div>
+        <Card text={e.text}/>
       ))}
+      </div>
     </div>
   );
 };
--- client/src/common/Card.js
+++ client/src/common/Card.js
@@ -4,10 +4,14 @@ class Card extends React.Component {
   render(){
     return(
       <div style={{
+        boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
         width: "225px",
         height: "350px",
         borderRadius: "10px",
-        backgroundColor: this.props.black ? "#000" : "#fff"
+        border: "1px solid #000",
+        backgroundColor: this.props.black ? "#000" : "#fff",
+        display: 'inline-block',
+        margin: '0px 10px'
       }}>
         <div style={{
           padding: "20px",
