8006a83f6703064338563204b02e3fe2a82a3b23
AaronCGoidel <agoidel2019@mka.org>
2018-12-01 12:45:11 -0500

    leader board progress

--- client/src/components/LeaderBoard.js
+++ client/src/components/LeaderBoard.js
@@ -3,9 +3,20 @@ import React from 'react';
 class LeaderBoard extends React.Component{
   render(){
     let rank = 0;
-    const items = this.props.players.map((player) => 
-    <li rank={`place_${++rank}`}>
-      {`${rank} ${player}`}
+    let items = this.props.players.sort((a, b) => (b.score - a.score)).map((player) => 
+    <li key={`place_${++rank}`} style={{
+      borderBottom: '1px solid black',
+      // padding: '5px'
+    }}>
+      {`${rank} ${player.name} `}<div style={{
+        height: '30px',
+        width: '30px',
+        backgroundColor: '#bbb',
+        borderRadius: '50%',
+        display: 'inline-block',
+        textAlign: 'center',
+        fontSize: '14px'
+      }}>{player.score}</div>
     </li>
     )
     return(
@@ -15,6 +26,7 @@ class LeaderBoard extends React.Component{
         color: "#000"
       }}>
         <ul style={{
+          padding: '0',
           listStyleType: "none"
         }}>
           {items}
