f966ee10e3771d6177b82a07a1345468996a0c89
AaronCGoidel <agoidel2019@mka.org>
2018-12-01 19:54:37 -0500

    horizontal scroll

--- client/src/client/Judgement.js
+++ client/src/client/Judgement.js
@@ -5,19 +5,21 @@ const Judgement = ({ prompt, choices, onSelect }) => {
   return (
     <div>
       <h1>Pick a pairing</h1>
-      {choices
-        .filter(e => e.choice !== null)
-        .map(e => (
-          <div style={{
+      <div style={{
             display: 'flex',
             flexWrap: 'nowrap',
             overflowX: 'scroll',
+            overflowY: 'none',
             height: '400px',
             whiteSpace: 'nowrap'
           }}>
+      {choices
+        .filter(e => e.choice !== null)
+        .map(e => (
+          
             <Card style={{whiteSpace: 'normal'}} key={e.id} text={prompt.text.replace(/_+/g, e.choice.text)} onClick={()=>onSelect(e)}/>
-          </div>
         ))}
+    </div>
     </div>
   );
 };
