8af9f4e3680d36c5abc7d3dbc687c963dae80157
AaronCGoidel <agoidel2019@mka.org>
2018-12-03 20:26:15 -0500

    new leaderboard format

--- client/src/host/LeaderBoard.js
+++ client/src/host/LeaderBoard.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./LeaderBoard.css";
 
-const LeaderBoardLine = ({ name, score }) => (
+const LeaderBoardLine = ({ place, name, score }) => (
   <div className="LeaderBoardLine">
+    <div className="num">{place}</div>
     <div className="name">{name}</div>
     <div className="score">{score}</div>
   </div>
@@ -10,18 +11,22 @@ const LeaderBoardLine = ({ name, score }) => (
 
 class LeaderBoard extends React.Component {
   render() {
+    let pos = 0;
     return (
       <div className="LeaderBoard">
         <h1>Players</h1>
+        <div className="container">
         {this.props.players
           .sort((a, b) => b.score - a.score)
           .map(player => (
             <LeaderBoardLine
               key={player.id}
+              place={++pos}
               name={player.name}
               score={player.score}
             />
           ))}
+        </div>
       </div>
     );
   }
